0342a59cd2dbce697ab5ec4ea38a8868055fa1f3
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-04-21 19:45:03 +0530

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './fileUpload/file-upload.component';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
